090f16be3209668e7e49da632d89f493f1f492ad
31847 <31847@bridgend.ac.uk>
2021-11-26 12:01:34 +0000

    complete

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
-def main():
-    #write your code below this line
 
-if __name__ == '__main__':
-    main()
+import random
+n = random.randint(0,9)
+
+
+guess = int(input('pick a number between 1 and 10: '))
+
+while n != 'guess':
+    print
+    if guess < n:
+        print ('guess is low')
+        guess = int(input('Enter a number between 1 and 10: '))
+    elif guess > n:
+        print ('guess is high')
+        guess = int(input('Enter a number betwen 1 and 10: '))
+    else:
+        print ('you did it')
+        break
+
